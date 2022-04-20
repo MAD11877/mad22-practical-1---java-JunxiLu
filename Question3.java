c45746645da0b49383d1509fcb1f8cfe7e9ba1ad
JunxiLu <S10219098@connect.np.edu.sg>
2022-04-20 12:06:58 +0800

    q3

--- Question3.java
+++ Question3.java
@@ -4,13 +4,12 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
+
     Scanner in = new Scanner(System.in);
+    int x = in.nextInt();
+    x = x * x;
+    System.out.print(x);
+    in.close();
     
   }
 }
